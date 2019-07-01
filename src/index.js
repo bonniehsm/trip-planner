3770d21ebed3dd8130143fa411e8832ab3379b6d
Bonnie <bonnie.hung@hotmail.ca>
2019-07-01 16:50:38 -0400

    Store - add comments; CreateTrip - update constructor & methods

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import App from "./js/components/container/App"
 import { createTrip } from "./js/actions";
 
 /** TESTING REDUX **/
-
 window.store = store;
 window.createTrip = createTrip;
 store.subscribe(() => console.log("Action dispatched!"));
 
-
+//element where our react app will mount
+const rootElement = document.getElementById("root");
 
 render(
   <Provider store={store}>
     <App/>
   </Provider>,
-  document.getElementById("root")
+  rootElement
 )
--- src/js/components/container/CreateTrip.js
+++ src/js/components/container/CreateTrip.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 
-class ViewDashboard extends Component {
+class CreateTrip extends Component {
   constructor(props){
     super(props);
 
-    //OBJECT SHAPE
+    //initializing state
     this.state = {
-      tripId: "",
-      destination: "",
-      departureAirportCode: "",
-      departureDateTime: "",
-      destinationArrivalDateTime: "",
-      returnAirportCode: "",
-      returnDateTime: "",
-      returnArrivalDateTime: "",
+      tripId: !this.props.tripId ? "" : this.props.tripId,
+      destination: !this.props.destination ? "" : this.props.destination,
+      departureAirportCode: !this.props.departureAirportCode? "" : this.props.departureAirportCode,
+      departureDateTime: !this.props.departureDateTime ? "" : this.props.departureDateTime,
+      destinationArrivalDateTime: !this.props.destinationArrivalDateTime? "" : this.props.destinationArrivalDateTime,
+      returnAirportCode: !this.props.returnAirportCode? "" : this.props.returnAirportCode,
+      returnDateTime: !this.props.returnDateTime? "" : this.props.returnDateTime,
+      returnArrivalDateTime: !this.props.returnArrivalDateTime? "" : this.props.returnArrivalDateTime,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+  }
+
+    //function that handles updating the state on input change
+    handleChange = (e) => {
+
     }
 
-  }
+    handleSubmit = (e) => {
+
+    }
+
+    //calls parent function to close the form
+    handleCancel = () => this.props.closeForm();
+
+
   render(){
     return(
       <div>
@@ -26,4 +43,4 @@ class ViewDashboard extends Component {
   }
 }
 
-export default ViewDashboard;
+export default CreateTrip;
