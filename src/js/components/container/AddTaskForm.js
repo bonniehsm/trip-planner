e96d99f130f45b99e16c7806ccaeece134ff646d
Bonnie <bonnie.hung@hotmail.ca>
2019-09-26 17:05:23 -0400

    creating Radio component in AddTaskForm

--- src/js/components/container/AddTaskForm.js
+++ src/js/components/container/AddTaskForm.js
@@ -7,13 +7,42 @@ import { connect } from "react-redux";
 const mapDispatchToProps = { addTask };
 
 
+function TaskRadio(){
+  const radio = props.radioType;
+  /*
+  <div className="add-task--done">
+    <label htmlFor="done">Completed? </label>
+    <input type="radio" name="done" defaultChecked={this.state.done} onChange={this.handleChange}/>Yes
+    <input type="radio" name="done" defaultChecked={!this.state.done} onChange={this.handleChange}/>No
+  </div>
+  */
+  return(
+    <div>
+      <label htmlFor={radio.name}>{radio.label}</label>
+
+    </div>
+  )
+}
+
 class AddTaskForm extends Component {
   constructor(props){
     super(props);
+    this.state = {
+      description: '',
+      done: false,
+      dueDate: '',
+      priority: false,
+    }
     this.createNewTask = this.createNewTask.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount(){
-    document.getElementById("add-task--form").onsubmit = function() {createNewTask()};
+  handleChange(e){
+    const target = e.target;
+    const name = target.name;
+    console.log(`handleChange() - name: ${name}, value: ${target.value}`);
+    this.setState({
+      [name]: target.value
+    })
   }
   createNewTask(e){
     e.preventDefault();
@@ -23,22 +52,39 @@ class AddTaskForm extends Component {
     this.props.toggleForm();
   }
   render(){
+    const taskDone = {
+      name: 'done',
+      label: 'Completed?',
+      options: [
+        {displayValue: 'Yes', checked: this.state.done},
+        {displayValue: 'No', checked: !this.state.done}
+      ]
+    };
+    const taskPriority = {
+      name: 'label',
+      label: 'Priority:',
+      options: [
+        {displayValue: 'High', checked: this.state.priority},
+        {displayValue: 'Low', checked: !this.state.priority}
+      ]
+    };
     return(
       <div className="add-task-form--container">
         <h4>Add Task Form</h4>
-        <form id="add-task--form">
+        <form id="add-task--form" onSubmit={this.createNewTask}>
           <div className="add-task--description">
-            <label htmlFor="task-description">Description: </label>
-            <input type="text" name="task-description"/>
+            <label htmlFor="description">Description: </label>
+            <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
           </div>
+          <TaskRadio radioType={taskDone}/>
           <div className="add-task--dueDate">
-            <label htmlFor="task-dueDate">Due Date: </label>
-            <input type="date" name="task-dueDate"/>
+            <label htmlFor="dueDate">Due Date: </label>
+            <input type="date" name="dueDate" value={this.state.dueDate} onChange={this.handleChange}/>
           </div>
           <div className="add-task--priority">
-            <label htmlFor="task-priority">Priority: </label>
-            <input type="radio" name="task-priority"/>High
-            <input type="radio" name="task-priority" defaultChecked/>Low
+            <label htmlFor="priority">Priority: </label>
+            <input type="radio" name="priority" defaultChecked={this.state.priority} onChange={this.handleChange}/>High
+            <input type="radio" name="priority" defaultChecked={!this.state.priority} onChange={this.handleChange}/>Low
           </div>
           <div className="add-task--buttons">
             <div className="add-task-buttons--submit">
