80875a27bcb1a0b13d2f0c8e6ce3b7e76d1787a2
Bonnie <bonnie.hung@hotmail.ca>
2019-10-05 18:28:50 -0400

    removed ViewDashboard component

--- src/js/components/container/App.js
+++ src/js/components/container/App.js
@@ -3,7 +3,6 @@ import Dashboard from "./Dashboard";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import ViewTripDetails from "./ViewTripDetails";
 import TripToDos from "./TripToDos";
-import ViewDashboard from "./ViewDashboard";
 import CreateTrip from "./CreateTrip";
 
 class App extends Component {
@@ -17,7 +16,6 @@ class App extends Component {
           <Dashboard/>
           <p><Link to="createTrip">Create Trip</Link></p>
           <p><Link to="todos">To-Do's</Link></p>
-          <Route path="/viewDashboard" component = {ViewDashboard}/>
           <Route path="/view/:id" component = {ViewTripDetails}/>
           <Route path="/todos" component = {TripToDos}/>
           <Route path="/createTrip" component = {CreateTrip}/>
