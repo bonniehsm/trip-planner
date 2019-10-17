13400dcbb83988a139941d69cb8ef8c344781859
Bonnie <bonnie.hung@hotmail.ca>
2019-10-17 16:43:08 -0400

    restructured project folder layout

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './client/src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
@@ -32,7 +32,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
+      template: "./client/src/index.html",
       filename: "./index.html"
     })
   ]
