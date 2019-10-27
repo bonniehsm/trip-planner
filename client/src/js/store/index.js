edc7fa65e7a523b9549a995eb48bf7219c2fb2c9
Bonnie <bonnie.hung@hotmail.ca>
2019-10-27 15:56:52 -0400

    added redux-logger to store middleware

--- client/src/js/store/index.js
+++ client/src/js/store/index.js
@@ -1,15 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from "../reducers/index";
+import logger from 'redux-logger';
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+import rootReducer from "../reducers/index";
 
-//thunk - ability to write action creators that return a function instead of an action e.g. fetch data from server
-const middlewares = [thunk];
-
-//the state of the application lives as a single, immutable object within the store
-  //as soon as the store receives an action, it triggers the reducer which returns the next state
-//applyMiddleware - add middlewares (3rd party extension point b/w dispatching an action & the moment it reaches the reducer)
-//composeWithDevTools - use redux dev tools along w/ custom middlewares
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+const middlewares = [thunk, logger];
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 
 export default store;
